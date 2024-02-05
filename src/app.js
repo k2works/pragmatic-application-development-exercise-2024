6705531bb6c009caba28b1417b5dfb792eb3e6df
Katuyuki Kakigi <kakimomokuri@gmail.com>
2024-02-05 14:01:21 +0900

    feat:エントリーポイント
    Update app initialization and remove unused sum function
    
    Deleted sum function and associated tests from app.js file. Updated App class in app.js to initialize with Todo App display on app element. Also, changed title in index.html for consistency. This declutters the project and aligns with the focus on Todo functionality.

--- src/app.js
+++ src/app.js
@@ -1,10 +1,11 @@
-console.log("app.js: loaded");
+console.log("App.js: loaded");
 export class App {
   constructor() {
     console.log("App initialized");
+    document.getElementById("app").innerHTML = `
+    <body>
+      <h1>Todo App</h1>
+    </body>
+    `;
   }
-}
-
-export function sum(a, b) {
-  return a + b;
 }
